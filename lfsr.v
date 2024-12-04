6c070595928885fa489c6ce8bb0dfbc521d1a4ae
liamogw <ogradyl2@miamioh.edu>
2024-12-04 14:11:32 -0500

    Create lfsr.v

---
+++ lfsr.v
@@ -0,0 +1,22 @@
+module lfsr(
+    input wire clk,
+    input wire rst,
+    output reg [15:0] rand
+);
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            rand <= 16'hACE1; // seed value
+        end else begin
+            rand[0] <= rand[15];
+            rand[1] <= rand[0];
+            rand[2] <= rand[1];
+            rand[3] <= rand[2];
+            rand[4] <= rand[3] ^ rand[15];
+            rand[5] <= rand[4] ^ rand[15];
+            rand[14:6] <= rand[13:5];
+            rand[15] <= rand[14] ^ rand[15];
+        end
+    end
+
+endmodule
