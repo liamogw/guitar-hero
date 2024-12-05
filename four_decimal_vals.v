04af6559830a7cfa38c861e094be5ab7233fc5eb
liamogw <ogradyl2@miamioh.edu>
2024-12-05 18:49:24 -0500

    Update four_decimal_vals.v

--- four_decimal_vals.v
+++ four_decimal_vals.v
@@ -6,12 +6,16 @@ module four_decimal_vals (
     output [6:0] seg7_dig3
 );
 
+//Declaring registers for each decimal value
 reg [3:0] result_one_digit;
+
 reg [3:0] result_ten_digit;
+
 reg [3:0] result_hundred_digit;
+
 reg [3:0] result_thousand_digit;
 
-/* convert the binary value into 4 signals */
+// convert the binary value into 4 signals
 always @(*) begin
     result_thousand_digit = (val / 1000) % 10;
     result_hundred_digit = (val / 100) % 10;
@@ -19,7 +23,7 @@ always @(*) begin
     result_one_digit = val % 10;
 end
 
-/* instantiate the modules for each of the seven seg decoders */
+// Instantiate each segment
 seven_segment dig0(
     .in(result_one_digit),
     .out(seg7_dig0)
