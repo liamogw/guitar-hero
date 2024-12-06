cbc9ffbae9267f3f0e281421349e905420f79768
liamogw <ogradyl2@miamioh.edu>
2024-12-06 16:04:22 -0500

    Final Note Generator

--- note_generator.v
+++ note_generator.v
@@ -7,13 +7,14 @@ module note_generator(
     output reg note_visible
 );
 
+//This is a basic note generator that we instantiate 3 times for the three different notes.
     parameter NOTE_WIDTH = 200;
+	 
     parameter NOTE_HEIGHT = 80;
 
     always @* begin
-        note_visible = note_active && 
-                       (h_count >= column_start) && (h_count < column_start + NOTE_WIDTH) &&
-                       (v_count >= note_y_position) && (v_count < note_y_position + NOTE_HEIGHT);
+	 
+        note_visible = note_active && (h_count >= column_start) && (h_count < column_start + NOTE_WIDTH) && (v_count >= note_y_position) && (v_count < note_y_position + NOTE_HEIGHT);
     end
 
 endmodule
