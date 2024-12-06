11b50825fc7e46b940db1d78f213c01e94eca7dc
liamogw <ogradyl2@miamioh.edu>
2024-12-06 16:08:12 -0500

    Final Seven Segment Display

--- seven_segment.v
+++ seven_segment.v
@@ -2,20 +2,34 @@ module seven_segment(
     input [3:0] in,
     output reg [6:0] out
 );
+//Same seven segment display we've used in labs
 
     always @(*) begin
+	 
         case(in)
+		  
             4'd0: out = 7'b1000000;
+				
             4'd1: out = 7'b1111001;
+				
             4'd2: out = 7'b0100100;
+				
             4'd3: out = 7'b0110000;
+				
             4'd4: out = 7'b0011001;
+				
             4'd5: out = 7'b0010010;
+				
             4'd6: out = 7'b0000010;
+				
             4'd7: out = 7'b1111000;
+				
             4'd8: out = 7'b0000000;
+				
             4'd9: out = 7'b0010000;
+				
             default: out = 7'b1111111;
+				
         endcase
     end
 
