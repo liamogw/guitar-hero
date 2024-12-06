f8a17a9c500e6d3dea316e13513dee29d0ce0a60
liamogw <ogradyl2@miamioh.edu>
2024-12-06 15:55:57 -0500

    Final Four Decimal Conversion

--- four_decimal_vals.v
+++ four_decimal_vals.v
@@ -1,22 +1,49 @@
 module four_decimal_vals (
-    input [15:0] val,
+    input [16:0] val,
     output [6:0] seg7_dig0,
     output [6:0] seg7_dig1,
     output [6:0] seg7_dig2,
-    output [6:0] seg7_dig3
+    output [6:0] seg7_dig3,
+	 output [6:0] seg7_neg
 );
 
+//Declare reg's for each digit place.
 reg [3:0] result_one_digit;
 reg [3:0] result_ten_digit;
 reg [3:0] result_hundred_digit;
 reg [3:0] result_thousand_digit;
+reg result_is_negative;
 
-/* convert the binary value into 4 signals */
+//Decare a two's compliment variable to use for negative values if needed.
+reg [16:0] twos_comp;
+
+//convert the binary value into 4 signals
+
 always @(*) begin
-    result_thousand_digit = (val / 1000) % 10;
-    result_hundred_digit = (val / 100) % 10;
-    result_ten_digit = (val / 10) % 10;
-    result_one_digit = val % 10;
+	 
+	 //If the biinary number is negative
+	 if (val[16] == 1'b1) begin
+	 
+			result_is_negative = 1'b1;
+			
+			twos_comp = -val;
+	 
+	 end else begin
+	 
+			result_is_negative = 1'b0;
+			
+			twos_comp = val;
+	 
+	 end
+
+    result_thousand_digit = (twos_comp / 1000) % 10;
+	 
+    result_hundred_digit = (twos_comp / 100) % 10;
+	 
+    result_ten_digit = (twos_comp / 10) % 10;
+	 
+    result_one_digit = twos_comp % 10;
+	 
 end
 
 /* instantiate the modules for each of the seven seg decoders */
@@ -38,6 +65,11 @@ seven_segment dig2(
 seven_segment dig3(
     .in(result_thousand_digit),
     .out(seg7_dig3)
+);
+
+seven_segment_negative neg(
+    .in(result_is_negative),
+    .out(seg7_neg)
 );
 
 endmodule
